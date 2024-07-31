a3314602dba6f0a0e9256f2bcdbb2f518bc1be64
MarkRyuo <malupajhonmark3035@gmail.com>
2024-07-31 21:51:40 +0800

    update herE

--- basics.py
+++ basics.py
@@ -1,30 +1,24 @@
+class Fighter:  # ? Blueprint
 
-
-
-class Fighter : # ? Blueprint 
-    
-    def __init__(self, healthPoints) : # ? This is the constructor 
+    def __init__(self, healthPoints):  # ? This is the constructor
         self.healthPoints = healthPoints
         # self.underAttack = underAttack
-    
-    def get_Hp(self) :
-        # todo create a logic here: if hero hp is lower than to 20 alert the player 
 
-        if self.healthPoints >= 100 :
+    def get_Hp(self):
+        # todo create a logic here: if hero hp is lower than to 20 alert the player
+
+        if self.healthPoints >= 100:
             self.healthPoints -= 20
             print(f"Your health is {self.healthPoints} your under attack")
 
-        if self.healthPoints <= 20 :
+        if self.healthPoints <= 20:
             print("Back up your health is low")
 
 
+def main():
 
+    fighterOne = Fighter(120)  # ? This is called intances
+    fighterOne.get_Hp()  # * 1st
 
-
-fighterOne = Fighter(120) # ? This is called intances
-fighterOne.get_Hp() # * 1st 
-
-fighterOne.get_Hp() # * 2nd 
-fighterOne.get_Hp() # * 3rd 
-
-
+    fighterOne.get_Hp()  # * 2nd
+    fighterOne.get_Hp()  # * 3rd
