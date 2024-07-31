d4d7560d058a5daca15c1104af882e9e3b2625f4
MarkRyuo <malupajhonmark3035@gmail.com>
2024-07-31 15:46:21 +0800

    update herE

--- basics.py
+++ basics.py
@@ -11,8 +11,16 @@ class Fighter : # ? Blueprint
         # todo create a logic here: if hero hp is lower than  to 10 alert the player 
         if self.healthPoints > 1000 :
             self.healthPoints -= 150
-            print(f"Your health is {self.healthPoints}")
+            print(f"Your health is {self.healthPoints} your under attack")
 
 
 fighterOne = Fighter(1000) # ? This is called intances
+fighterOne.get_Hp()
+fighterOne.get_Hp()
+fighterOne.get_Hp()
+fighterOne.get_Hp()
+fighterOne.get_Hp()
+fighterOne.get_Hp()
+fighterOne.get_Hp()
+fighterOne.get_Hp()
 fighterOne.get_Hp()
