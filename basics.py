48d07be49cf512294f8a73d1ca15173a79e6a70a
MarkRyuo <malupajhonmark3035@gmail.com>
2024-07-31 15:34:29 +0800

    commit

--- basics.py
+++ basics.py
@@ -3,8 +3,10 @@
 
 class Fighter : # ? Blueprint 
     
-    def __init__(self, healthPoints) :
+    def __init__(self, healthPoints) : # ? This is the constructor 
         self.healthPoints = healthPoints
+    
 
+    
 
 fighterOne = Fighter() # ? This is called intances
