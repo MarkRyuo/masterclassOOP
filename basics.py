7357b0b69e76363d5cdfbf442df7478bafc42479
MarkRyuo <malupajhonmark3035@gmail.com>
2024-07-31 15:54:04 +0800

    update herE

--- basics.py
+++ basics.py
@@ -8,11 +8,7 @@ class Fighter : # ? Blueprint
         # self.underAttack = underAttack
     
     def get_Hp(self) :
-        # todo create a logic here: if hero hp is lower than  to 10 alert the player 
-
-        if self.healthPoints < 10 :
-            print("Back up your health is low")
-
+        # todo create a logic here: if hero hp is lower than to 10 alert the player 
 
         if self.healthPoints > 1000 :
             self.healthPoints -= 150
