3ff4c78dd157b7c39a179559481a6245f611a892
MarkRyuo <malupajhonmark3035@gmail.com>
2024-08-01 18:21:46 +0800

    update herE

--- Basics/classmethod.py
+++ Basics/classmethod.py
@@ -3,3 +3,11 @@ class Marksman :
     
     def __init__(self, damage, hp) :
         self.damage = damage 
+        self.hp = hp 
+
+
+    def call(self) :
+        print(
+            f"Damage: {self.damage} "
+            
+            )
