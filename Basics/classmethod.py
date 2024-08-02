23b3380ee3d4139b3408ee07e93bc6d164af3be4
MarkRyuo <malupajhonmark3035@gmail.com>
2024-08-02 19:41:19 +0800

    update

--- Basics/classmethod.py
+++ Basics/classmethod.py
@@ -12,7 +12,7 @@ class Marksman :
         print(f"Hp: {self.hp}")
     
 
-    @classmethod # ? This is a decorator 
+    @classmethod # ? This is a decorator (Most of the time class method is to provide alternative to the init method)
     def clsmethod(cls) :
         print("I'm class method")
 
@@ -20,9 +20,11 @@ class Marksman :
 
 def main() :
 
-    mm = Marksman(damage=400, hp=1000) #? This is good for practices
+    mm = Marksman(damage=400, hp=1000) #? This is good for practices    
     mm.call()
 
     mm.clsmethod()
 
 main()
+
+
