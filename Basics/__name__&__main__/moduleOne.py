b429e9ff7a1b656d7ad1436b7d18872b7371e3ea
MarkRyuo <malupajhonmark3035@gmail.com>
2024-08-03 22:10:59 +0800

    update herE

--- Basics/__name__&__main__/moduleOne.py
+++ Basics/__name__&__main__/moduleOne.py
@@ -9,7 +9,7 @@ def call(name) :
 
 if __name__ == '__main__' :
     print("This is ModuleOne")
-    print(call("Riyuo"))
+    print(call("Riyuo")) # ? Riyuo is called argument
 else : 
     print("Success...")
     print("Loading main......")
