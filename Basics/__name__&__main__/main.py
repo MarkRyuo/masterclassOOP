4777088ff99693094371c1269d8b4533268f4ded
MarkRyuo <malupajhonmark3035@gmail.com>
2024-08-03 22:06:08 +0800

    update herE

--- Basics/__name__&__main__/main.py
+++ Basics/__name__&__main__/main.py
@@ -3,7 +3,6 @@ import moduleOne
 # ? In order to prevent code an import to be executed when it is imported we have to add a guard is the guard is if __name__ == '__main__' : statement 
 
 def main() :
-
     moduleOne.call("moda")
 
 
