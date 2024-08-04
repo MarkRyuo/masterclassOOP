bc1f4f8373dd791a60875860c166f9aa4a545d1c
MarkRyuo <malupajhonmark3035@gmail.com>
2024-08-04 17:22:30 +0800

    Aug 4 2024 Update

--- Basics/instance&static&classmethod/instancemethod.py
+++ Basics/instance&static&classmethod/instancemethod.py
@@ -5,9 +5,15 @@
 
 class Animal :
 
-    def __init__(self, animalName: str , animalAge) :
+    def __init__(self, animalName: str , animalAge: int) :
         self.animalName = animalName
         self.animalAge = animalAge 
     
     def display(self) :
         return f"Pet name: {self.animalName} is {self.animalAge} years old!"
+
+
+
+
+if __name__ == '__main__' :
+    A1 = Animal("")
