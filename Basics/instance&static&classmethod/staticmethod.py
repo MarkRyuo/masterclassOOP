745e5eb73645ae5c4d89590a37e75dd79c3d29b5
MarkRyuo <malupajhonmark3035@gmail.com>
2024-08-04 16:26:09 +0800

    update herE

--- Basics/instance&static&classmethod/staticmethod.py
+++ Basics/instance&static&classmethod/staticmethod.py
@@ -9,4 +9,12 @@ class Person :
     
     def description(self) :
         return f"{self.name} is {self.age} years old!"
+    
+    @staticmethod
+    def add_numbers(*numbers: float) -> float:
+        return sum(numbers)
 
+
+if __name__ == '__main__' :
+    p1 = Person("moda", 21)
+    
