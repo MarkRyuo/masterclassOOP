3530cf5f60b7793fe2d692d004b7f1be02293e7e
MarkRyuo <malupajhonmark3035@gmail.com>
2024-08-04 16:04:33 +0800

    Aug 4 2024 Update

--- Basics/instance&static&classmethod/classmethod.py
+++ Basics/instance&static&classmethod/classmethod.py
@@ -2,5 +2,14 @@ from typing import Self
 from datetime import date 
 
 
+class Person :
+
+    def __init__(self, name: str, age: int ) :
+        self.name = name 
+        self.age = age 
+    
+    
+
+
 
 
