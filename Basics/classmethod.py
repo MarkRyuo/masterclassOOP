3036c5abdc3248ce50bcb478ef17a7b8e762e095
MarkRyuo <malupajhonmark3035@gmail.com>
2024-08-05 17:42:38 +0800

    updated in Aug 5 2024

--- Basics/classmethod.py
+++ Basics/classmethod.py
@@ -4,6 +4,4 @@
 
 class Marksman :
     
-    def __init__(self, damage: int, hp: int) : # ? Constructor 
-        self.damage = damage # ? public Attribute 
-        self.hp =``
+    def __init__(self, damage: int, hp: 
