74953cf3b9a94eb1dc393326d3a94cca63500b7b
MarkRyuo <malupajhonmark3035@gmail.com>
2024-08-07 17:38:57 +0800

    Update Aug 7 2024

--- PracticeMode/static&classmethod/Animals.py
+++ PracticeMode/static&classmethod/Animals.py
@@ -9,7 +9,10 @@ from abc import ABC, abstractmethod
 
 class Animal(ABC) :
 
+
+
     def __init__(self, animalName: str, animalType: str, animalAge: int ) :
-        self.animalName = animalName 
+        self.animalName = animalName # ? A public class  
         self.animalType = animalType
         self.animalAge = animalAge
+    
