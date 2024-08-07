2dc3b9a53660c3f7ad85d7baf5cb40b59dfe894c
MarkRyuo <malupajhonmark3035@gmail.com>
2024-08-07 17:49:09 +0800

    Update Aug 7 2024

--- PracticeMode/static&classmethod/Animals.py
+++ PracticeMode/static&classmethod/Animals.py
@@ -3,13 +3,14 @@ from abc import ABC, abstractmethod
 
 
 # Todo create animal system can calculate the age of dog base in birth 
-# Todo kailangan m
+# Todo kailangan mabilang ang mga hayup na nakalagay sa system 
 
 
 
 class Animal(ABC) :
 
 
+    num_of_animals = 0
 
     def __init__(self, animalName: str, animalType: str, animalAge: int ) :
         self.animalName = animalName # ? A public class  
@@ -19,5 +20,7 @@ class Animal(ABC) :
     @abstractmethod  # * Create abstract method for passing in method talk() in the animals
     def talk(self) :
         pass  
+    
+
 
     
