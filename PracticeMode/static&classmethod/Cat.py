3556a5f42bc33dd11dcf36696ccd5197ef0f8daa
MarkRyuo <malupajhonmark3035@gmail.com>
2024-08-08 17:44:53 +0800

    update Aug 8 2024

---
+++ PracticeMode/static&classmethod/Cat.py
@@ -0,0 +1,16 @@
+from Animals import Animal
+
+
+class Dog(Animal) :
+
+    def __init__(self, animalName: str, animalType: str, dateofbirth: int) :
+        super().__init__(animalName, animalType, dateofbirth) #* Importing the super class 
+    
+
+    def talk(self): 
+        return f"{self.animalName} said Woof! Woof!"
+
+    
+
+    
+
