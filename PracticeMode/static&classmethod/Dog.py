100329253f1c1a325d6f3dfce01204057974b622
MarkRyuo <malupajhonmark3035@gmail.com>
2024-08-08 17:12:57 +0800

    update Aug 8 2024

--- PracticeMode/static&classmethod/Dog.py
+++ PracticeMode/static&classmethod/Dog.py
@@ -4,7 +4,6 @@ from Animals import Animal
 class Dog(Animal) :
 
     def __init__(self, animalName: str, animalType: str, dateofbirth: int) :
-        super().__init__(animalName, animalType, dateofbirth)
+        super().__init__(animalName, animalType, dateofbirth) #* Importing the super class 
     
 
-    
