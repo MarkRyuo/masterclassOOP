69dc6c060926d8cb8aaebc9480451c904ef40fad
MarkRyuo <malupajhonmark3035@gmail.com>
2024-08-10 22:24:12 +0800

    update Aug 10 2024

--- Calculator/calculate.py
+++ Calculator/calculate.py
@@ -3,8 +3,7 @@
 
 class Calculate() :
 
-    def __init__(self, num1: int, num2: int) -> None :
-        self.num1 = num1
-        self.num2 = num2
+    def __init__(self) -> None :
+        pass
     
-    def Get_Number(self)
+    def Get_Number(self) :
