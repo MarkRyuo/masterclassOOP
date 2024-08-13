1daea6c745a9d8703701286ebf3502f0cd43239c
MarkRyuo <malupajhonmark3035@gmail.com>
2024-08-13 22:08:13 +0800

    Update Aug 13 2024

--- Calculator/main.py
+++ Calculator/main.py
@@ -8,3 +8,4 @@ if __name__ == '__main__' :
         print(
             f" Num1: {get_the_Number.Get_Num1()} \n Num2: {get_the_Number.Get_Num2()}"
             )
+        print(get_the_Number.Get_Num1() + )
